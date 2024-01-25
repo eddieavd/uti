fe973141062f7dc4f9b7cbc54da31b9b17059a32
Edhem Avdagic <edhemavdagic@gmail.com>
2024-01-25 15:29:16 +0100

    update benchmarks

--- bench/gbench_uti.hpp
+++ bench/gbench_uti.hpp
@@ -30,6 +30,22 @@ static void bm_push_back ( benchmark::State & state )
 }
 
 template< typename T >
+static void bm_push_back_nontrivial ( benchmark::State & state )
+{
+        for( auto _ : state )
+        {
+                T container;
+
+                for( ssize_t i = 0; i < state.range( 0 ); ++i )
+                {
+                        container.emplace_back();
+                }
+                benchmark::ClobberMemory();
+                benchmark::DoNotOptimize( container );
+        }
+}
+
+template< typename T >
 static void bm_push_back_reserved ( benchmark::State & state )
 {
         for( auto _ : state )
@@ -40,6 +56,23 @@ static void bm_push_back_reserved ( benchmark::State & state )
                 for( ssize_t i = 0; i < state.range( 0 ); ++i )
                 {
                         container.push_back( 1024 );
+                }
+                benchmark::ClobberMemory();
+                benchmark::DoNotOptimize( container );
+        }
+}
+
+template< typename T >
+static void bm_push_back_reserved_nontrivial ( benchmark::State & state )
+{
+        for( auto _ : state )
+        {
+                T container;
+                container.reserve( state.range( 0 ) );
+
+                for( ssize_t i = 0; i < state.range( 0 ); ++i )
+                {
+                        container.emplace_back();
                 }
                 benchmark::ClobberMemory();
                 benchmark::DoNotOptimize( container );
