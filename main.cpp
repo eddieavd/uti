d62366d0be068be792857ef61c829b4f6c2fa0cb
Edhem Avdagic <edhemavdagic@gmail.com>
2024-01-26 21:46:13 +0100

    update main

--- main.cpp
+++ main.cpp
@@ -9,18 +9,6 @@
 
 int main ()
 {
-        uti::vector vec1d( 4,    1  );
-        uti::vector vec2d( 4, vec1d );
-
-        for( uti::ssize_t i = 0; i < 4; ++i )
-        {
-                for( uti::ssize_t j = 0; j < 4; ++j )
-                {
-                        std::cout << vec2d.at( i, j ) << " ";
-                }
-                std::cout << "\n";
-        }
-
 
         return 0;
 }
