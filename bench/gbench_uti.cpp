efd338732f9d5acd01228918beab7f0187464a2c
Edhem Avdagic <edhemavdagic@gmail.com>
2024-01-27 10:29:52 +0100

    update benchmarks

--- bench/gbench_uti.cpp
+++ bench/gbench_uti.cpp
@@ -12,14 +12,14 @@
 using namespace uti::bench ;
 
 
-BENCHMARK( bm_push_back< std::vector< int > > )->RangeMultiplier( 2 )->Range( 256, 256 << 16 )->Unit( benchmark::kMicrosecond );
-BENCHMARK( bm_push_back< uti::vector< int > > )->RangeMultiplier( 2 )->Range( 256, 256 << 16 )->Unit( benchmark::kMicrosecond );
+BENCHMARK( bm_push_back< std::vector< int > > )->RangeMultiplier( 2 )->Range( 256, 256 << 8 )->Unit( benchmark::kMicrosecond );
+BENCHMARK( bm_push_back< uti::vector< int > > )->RangeMultiplier( 2 )->Range( 256, 256 << 8 )->Unit( benchmark::kMicrosecond );
 
-BENCHMARK( bm_push_back_nontrivial< std::vector< std::string > > )->RangeMultiplier( 2 )->Range( 256, 256 << 12 )->Unit( benchmark::kMicrosecond );
-BENCHMARK( bm_push_back_nontrivial< uti::vector< std::string > > )->RangeMultiplier( 2 )->Range( 256, 256 << 12 )->Unit( benchmark::kMicrosecond );
+BENCHMARK( bm_push_back_nontrivial< std::vector< std::string > > )->RangeMultiplier( 2 )->Range( 256, 256 << 8 )->Unit( benchmark::kMicrosecond );
+BENCHMARK( bm_push_back_nontrivial< uti::vector< std::string > > )->RangeMultiplier( 2 )->Range( 256, 256 << 8 )->Unit( benchmark::kMicrosecond );
 
-BENCHMARK( bm_push_back_reserved< std::vector< int > > )->RangeMultiplier( 2 )->Range( 256, 256 << 16 )->Unit( benchmark::kMicrosecond );
-BENCHMARK( bm_push_back_reserved< uti::vector< int > > )->RangeMultiplier( 2 )->Range( 256, 256 << 16 )->Unit( benchmark::kMicrosecond );
+BENCHMARK( bm_push_back_reserved< std::vector< int > > )->RangeMultiplier( 2 )->Range( 256, 256 << 8 )->Unit( benchmark::kMicrosecond );
+BENCHMARK( bm_push_back_reserved< uti::vector< int > > )->RangeMultiplier( 2 )->Range( 256, 256 << 8 )->Unit( benchmark::kMicrosecond );
 
-BENCHMARK( bm_push_back_reserved_nontrivial< std::vector< std::string > > )->RangeMultiplier( 2 )->Range( 256, 256 << 12 )->Unit( benchmark::kMicrosecond );
-BENCHMARK( bm_push_back_reserved_nontrivial< uti::vector< std::string > > )->RangeMultiplier( 2 )->Range( 256, 256 << 12 )->Unit( benchmark::kMicrosecond );
+BENCHMARK( bm_push_back_reserved_nontrivial< std::vector< std::string > > )->RangeMultiplier( 2 )->Range( 256, 256 << 8 )->Unit( benchmark::kMicrosecond );
+BENCHMARK( bm_push_back_reserved_nontrivial< uti::vector< std::string > > )->RangeMultiplier( 2 )->Range( 256, 256 << 8 )->Unit( benchmark::kMicrosecond );
