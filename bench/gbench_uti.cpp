d4d11844bad936699616fe1e9c50e8db0e85546e
Edhem Avdagic <edhemavdagic@gmail.com>
2024-01-27 12:17:31 +0100

    update benchmarks

--- bench/gbench_uti.cpp
+++ bench/gbench_uti.cpp
@@ -23,3 +23,9 @@ BENCHMARK( bm_push_back_reserved_trivial< uti::vector< int > > )->RangeMultiplie
 
 BENCHMARK( bm_push_back_reserved_nontrivial< std::vector< std::string > > )->RangeMultiplier( 2 )->Range( 1024, 1024 << 12 )->Unit( benchmark::kMicrosecond );
 BENCHMARK( bm_push_back_reserved_nontrivial< uti::vector< std::string > > )->RangeMultiplier( 2 )->Range( 1024, 1024 << 12 )->Unit( benchmark::kMicrosecond );
+
+BENCHMARK( bm_copy_container_trivial< std::vector< int > > )->RangeMultiplier( 2 )->Range( 1024, 1024 << 12 )->Unit( benchmark::kMicrosecond );
+BENCHMARK( bm_copy_container_trivial< uti::vector< int > > )->RangeMultiplier( 2 )->Range( 1024, 1024 << 12 )->Unit( benchmark::kMicrosecond );
+
+BENCHMARK( bm_copy_container_nontrivial< std::vector< std::string > > )->RangeMultiplier( 2 )->Range( 1024, 1024 << 12 )->Unit( benchmark::kMicrosecond );
+BENCHMARK( bm_copy_container_nontrivial< uti::vector< std::string > > )->RangeMultiplier( 2 )->Range( 1024, 1024 << 12 )->Unit( benchmark::kMicrosecond );
--- bench/gbench_uti.hpp
+++ bench/gbench_uti.hpp
@@ -79,5 +79,39 @@ static void bm_push_back_reserved_nontrivial ( benchmark::State & state )
         }
 }
 
+template< typename T >
+static void bm_copy_container_trivial ( benchmark::State & state )
+{
+        T container;
+        for( ssize_t i = 0; i < state.range( 0 ); ++i )
+        {
+                container.push_back( i );
+        }
+        for( auto _ : state )
+        {
+                T copy( container );
+
+                benchmark::ClobberMemory();
+                benchmark::DoNotOptimize( copy );
+        }
+}
+
+template< typename T >
+static void bm_copy_container_nontrivial ( benchmark::State & state )
+{
+        T container;
+        for( ssize_t i = 0; i < state.range( 0 ); ++i )
+        {
+                container.emplace_back();
+        }
+        for( auto _ : state )
+        {
+                T copy( container );
+
+                benchmark::ClobberMemory();
+                benchmark::DoNotOptimize( copy );
+        }
+}
+
 
 } // namespace uti::bench
